a6aef9daae798f6400f3e354ed7799f2339b83d0
weikangYeo <weikang203@gmail.com>
2025-11-08 00:44:54 +0800

    Refactor code and complete main flow (Before TEST)

--- main.go
+++ main.go
@@ -4,37 +4,89 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"strconv"
+	"time"
 )
 
+var start = time.Now()
+
 func main() {
-	numPhilosophers, timeToEat, timeToSleep, timeTimeToDie, numberOfTimesToEat, error := getFlags()
+	numPhilosophers, timeToEat, timeToSleep, timeToDie, totalEatCountToStopGame, error := getFlags()
 
 	if error != nil {
 		fmt.Println(error)
 		return
 	}
 
-}
-
-func createPhilosophers(numPhilosophers int) []Philosopher {
+	// init forks
+	forks := make([]Fork, numPhilosophers)
+	for i := 0; i < numPhilosophers; i++ {
+		forks[i] = Fork{
+			isUsed: false,
+		}
+	}
+	// init philosophers
 	philosophers := make([]Philosopher, numPhilosophers)
 	for i := 0; i < numPhilosophers; i++ {
 		philosophers[i] = Philosopher{
-			name: fmt.Sprintf("Philosopher %d", i),
-			leftFork: i,
-			rightFork: (i + 1) % numPhilosophers,
-			state: "thinking"
+			name:          strconv.Itoa(i),
+			leftFork:      &forks[i],
+			rightFork:     &forks[(i+1)%numPhilosophers],
+			gameStartTime: start,
 		}
 	}
-	return philosophers
+
+	// use index instead of range value, because it will create a copy of value here which wanted to avoid.
+	for i := range philosophers {
+		go philosophers[i].StartRoutine(timeToEat, timeToSleep)
+	}
+
+	isGameStopped := false
+	for !isGameStopped {
+		totalEatCount := 0
+		// loop to check status
+		for i := range philosophers {
+			// check if any philosophers died
+			isGameStopped = checkPhiloDead(philosophers, i, timeToDie)
+			if isGameStopped {
+				break
+			}
+			philosophers[i].lock.Lock()
+			totalEatCount += philosophers[i].eatCount
+			philosophers[i].lock.Unlock()
+		}
+
+		if totalEatCount >= totalEatCountToStopGame {
+			isGameStopped = true
+		}
+	}
 }
 
-func getFlags() (int, uint, uint, uint, uint, error) {
+// checkPhiloDead return true
+func checkPhiloDead(philosophers []Philosopher, i int, timeToDie int64) bool {
+	philosophers[i].lock.Lock()
+	lastMealDuration := time.Since(philosophers[i].lastMealTime)
+	if timeToDie >= lastMealDuration.Milliseconds() {
+		philosophers[i].lock.Unlock()
+		philosophers[i].killSignal <- true
+		// stop all other thread
+		for k := range philosophers {
+			if k != i {
+				philosophers[i].stopSignal <- true
+			}
+		}
+		return true
+	}
+	philosophers[i].lock.Unlock()
+	return false
+}
+
+func getFlags() (int, int64, int64, int64, int, error) {
 	numPhilosophers := flag.Int("number_of_philosophers", 5, "number of philosophers")
-	timeToEat := flag.Uint("time_to_eat", 1000, "time to eat in milliseconds")
-	timeToSleep := flag.Uint("time_to_sleep", 1000, "time to sleep in milliseconds")
-	timeTimeToDie := flag.Uint("time_to_die", 1000, "time to die in milliseconds")
-	numberOfTimesToEat := flag.Uint("number_of_times_to_eat", 0, "number of times to eat")
+	timeToEat := flag.Int64("time_to_eat", 1000, "time to eat in milliseconds")
+	timeToSleep := flag.Int64("time_to_sleep", 1000, "time to fallSleep in milliseconds")
+	timeTimeToDie := flag.Int64("time_to_die", 1000, "time to die in milliseconds")
+	numberOfTimesToEat := flag.Int("number_of_times_to_eat", 0, "number of times to eat")
 	flag.Parse()
 
 	if *numberOfTimesToEat == 0 {
@@ -46,7 +98,7 @@ func getFlags() (int, uint, uint, uint, uint, error) {
 	}
 
 	if *timeToSleep == 0 {
-		return 0, 0, 0, 0, 0, errors.New("time to sleep must be greater than 0")
+		return 0, 0, 0, 0, 0, errors.New("time to fallSleep must be greater than 0")
 	}
 
 	if *timeTimeToDie == 0 {
